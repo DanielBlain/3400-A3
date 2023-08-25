4f11449cefd9a4a82702798e55b222a5ecec529c
DanielBlain <dblain2@my.bcit.ca>
2023-08-25 15:24:57 -0700

    Positioning with grid-area functional, do I really need DanCalcButton now?

--- src/components/DanCalc.js
+++ src/components/DanCalc.js
@@ -1,19 +1,18 @@
-import Display from './Display';
 import DanCalcButton from './DanCalcButton';
 
 const DanCalc = ({calculatorButtons}) => {
     return (
     <article className='DanCalc-panel'>
-        <Display />
-        <div className='DanCalc-keyboard'>
-            {calculatorButtons.map(nextButton => (
-                <DanCalcButton
-                    buttonClass={"DanCalc-" + nextButton.className + "-button"}
-                    buttonValue={nextButton.value}
-                    buttonText={nextButton.text}
-                />
-            ))}
+        <div className='DanCalc-display'>
+            Display
         </div>
+        {calculatorButtons.map(nextButton => (
+            <DanCalcButton key={nextButton.value}
+                buttonClass={"DanCalc-" + nextButton.className + "-button"}
+                buttonValue={nextButton.value}
+                buttonText={nextButton.text}
+            />
+        ))}
     </article>
   )
 }
